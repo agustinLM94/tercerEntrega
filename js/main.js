cf90f39a3c48f3cff20c363b45226f23b940aa77
aguslopez <agustin.emanuel.lopez@gmail.com>
2024-04-12 07:55:30 -0300

    hola

--- js/main.js
+++ js/main.js
@@ -105,3 +105,36 @@ function reiniciarJuego() {
 
     instrucciones.innerText = "El primero en llegar a 5 puntos gana."
 }
+
+
+
+const botonColorMode = document.querySelector("#color-mode");
+const body = document.body;
+
+let darkMode = localStorage.getItem("dark-mode");
+
+function activarDarkMode() {
+    body.classList.add("dark-mode");
+    localStorage.setItem("dark-mode", "activado");
+}
+
+function desactivarDarkMode() {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("dark-mode", "desactivado");
+}
+
+if (darkMode === "activado") {
+    activarDarkMode();
+} else {
+    desactivarDarkMode();
+}
+
+botonColorMode.addEventListener("click", () => {
+    darkMode = localStorage.getItem("dark-mode");
+
+    if (darkMode === "activado") {
+        desactivarDarkMode();
+    } else {
+        activarDarkMode();
+    }
+})
